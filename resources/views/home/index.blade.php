c9c73f27b367e2fca668c6de4f80fd451b926707
Jessby <1109995448@qq.com>
2018-02-02 15:55:14 +0800

    tijiao

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -82,7 +82,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -93,7 +93,7 @@
                             <h6><a href="{{url('detial')}}">Sed ut perspiciati</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -114,7 +114,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc1.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc1.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc1.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -146,7 +146,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc2.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc2.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc2.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -157,7 +157,7 @@
                             <h6><a href="{{url('detial')}}">Sed ut perspiciati</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -178,7 +178,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc3.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc3.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc3.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -189,7 +189,7 @@
                             <h6><a href="{{url('detial')}}">On the other</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -213,7 +213,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc4.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc4.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc4.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -224,7 +224,7 @@
                             <h6><a href="{{url('detial')}}">On the other</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -245,7 +245,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc5.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc5.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc5.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -256,7 +256,7 @@
                             <h6><a href="{{url('detial')}}">Sed ut perspiciati</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -277,7 +277,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc6.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc6.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc6.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -288,7 +288,7 @@
                             <h6><a href="{{url('detial')}}">At vero eos</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -309,7 +309,7 @@
                     <div class="pro-img">
                         <img src="{{ asset('homes/images/pc7.jpg') }}" class="img-responsive" alt="">
                         <div class="zoom-icon ">
-                        <a class="picture" href="images/pc7.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
+                        <a class="picture" href="{{ asset('homes/images/pc7.jpg') }}" rel="title" class="b-link-stripe b-animate-go  thickbox"><i class="glyphicon glyphicon-search icon "></i></a>
                         <a href="{{url('detial')}}"><i class="glyphicon glyphicon-menu-right icon"></i></a>
                         </div>
                         </div>
@@ -320,7 +320,7 @@
                             <h6><a href="{{url('detial')}}">Sed ut perspiciati</a></h6>
                             </div>
                             <div class="img item_add">
-                                <a href="#"><img src="{{ asset('homes/images/ca..png') }}" alt=""></a>
+                                <a href="#"><img src="{{ asset('homes/images/ca.png') }}" alt=""></a>
                             </div>
                             <div class="clearfix"></div>
                             </div>
@@ -343,16 +343,16 @@
             <!--brand-->
             <div class="brand">
                 <div class="col-md-3 brand-grid">
-                    <img src="{{ asset('homes/images/ic..png') }}" class="img-responsive" alt="">
+                    <img src="{{ asset('homes/images/ic.png') }}" class="img-responsive" alt="">
                 </div>
                 <div class="col-md-3 brand-grid">
-                    <img src="{{ asset('homes/images/ic1..png') }}" class="img-responsive" alt="">
+                    <img src="{{ asset('homes/images/ic1.png') }}" class="img-responsive" alt="">
                 </div>
                 <div class="col-md-3 brand-grid">
-                    <img src="{{ asset('homes/images/ic2..png') }}" class="img-responsive" alt="">
+                    <img src="{{ asset('homes/images/ic2.png') }}" class="img-responsive" alt="">
                 </div>
                 <div class="col-md-3 brand-grid">
-                    <img src="{{ asset('homes/images/ic3..png') }}" class="img-responsive" alt="">
+                    <img src="{{ asset('homes/images/ic3.png') }}" class="img-responsive" alt="">
                 </div>
                 <div class="clearfix"></div>
             </div>
